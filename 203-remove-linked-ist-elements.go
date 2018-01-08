b6a48a2256a9539c4a0c526167c3a2f463b1f4a3
frankobe <mua08p@gmail.com>
2018-01-07 22:46:11 -0800

    init commit

---
+++ 203-remove-linked-ist-elements.go
@@ -0,0 +1,21 @@
+package removeElements
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func removeElements(head *ListNode, val int) *ListNode {
+	fakeHead := &ListNode{Val:0, Next:head}
+	ptr := fakeHead
+	for  ptr != nil && ptr.Next != nil {
+			if ptr.Next.Val == val {
+			 ptr.Next = ptr.Next.Next
+			} else {
+					ptr = ptr.Next
+			}
+	}
+
+	return fakeHead.Next
+}
