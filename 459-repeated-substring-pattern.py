84901da8918c11caecd294bcecf2179065d3a7a1
Meng Wei <weimeng@weimengs-MacBook-Air.local>
2018-01-07 23:28:44 -0800

    back to VIM

---
+++ 459-repeated-substring-pattern.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        def shift(s, i):
+            return s[i:]+s[0:i]
+        """
+        :type s: str
+        :rtype: bool
+        """
+        for i in range(1, len(s)/2+1):
+            if len(s)%i==0:
+                ss = shift(s,i)
+                if ss==s:
+                    return True
+        return False
